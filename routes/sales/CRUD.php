37979063cd57c1f9ff4f07feb3d2372750011d19
devmostafa80 <devmostafa80@gmail.com>
2022-02-27 14:13:57 +0200

    mohamed gamal 27-06

--- routes/sales/CRUD.php
+++ routes/sales/CRUD.php
@@ -6,18 +6,15 @@ use Illuminate\Support\Facades\Route;
     Route::resource('client', 'ClientController');
     Route::get('client-search', 'ClientController@search')->name('client.search');
 
-<<<<<<< HEAD
     // Ticket
     Route::resource('ticket', 'TicketController');
     Route::get('calcCapacity', 'TicketController@calcCapacity')->name('calcCapacity');
     Route::POST('storeModels', 'TicketController@storeModels')->name('storeModels');
     Route::POST('storeRevTicket', 'ReservationController@storeRevTicket')->name('storeRevTicket');
 
-=======
-// Reservation
-Route::get('searchForReservations', 'ReservationController@searchForReservations')->name('searchForReservations');
-Route::POST('delete_reservation', 'ReservationController@delete_reservation')->name('delete_reservation');
->>>>>>> 5841603f487d8a67c09bbeab115f347c9cf4125e
+    // Reservation
+    Route::get('searchForReservations', 'ReservationController@searchForReservations')->name('searchForReservations');
+    Route::POST('delete_reservation', 'ReservationController@delete_reservation')->name('delete_reservation');
 
     //==================================== Group ================================
     Route::resource('familyAccess', 'FamilyAccessController');
